16d4f403d0cf84269d941bd13166356e16dcb1fe
Jay Mandane <jvergel.mandane@gmail.com>
2023-12-22 19:22:08 +0800

    Added a header and footer to the pause screen

--- pages/pause.py
+++ pages/pause.py
@@ -6,6 +6,7 @@ import text_utils as tu
 
 class PausePage(object):
     def __init__(self, screen_size):
+        self.screen_size = screen_size
         self.surface = pygame.surface.Surface(screen_size)
         self.num_items = 2
         self.curr_item_index = 0
@@ -36,12 +37,28 @@ class PausePage(object):
     def draw(self):
         self.surface.fill(colors.beige)
 
+        sentence_surface = tu.regular_text(
+            colors.blue,
+            'The game is paused.'
+        )
+        self.surface.blit(
+            sentence_surface,
+            (self.screen_size[0]/2 - sentence_surface.get_size()[0]/2, 20),
+        )
+
         self.surface.blit(
             tu.regular_text(self._item_color(0), 'Resume'),
-            (40, 40),
+            (40, tu.header_height()),
         )
 
         self.surface.blit(
             tu.regular_text(self._item_color(1), 'Main menu'),
-            (40, 40 + tu.line_size(1.5)),
+            (40, tu.header_height() + tu.line_size(1.5)),
+        )
+
+        footer_text = '\u2191\u2193: Highlight, <Enter>: Select, '+ \
+          '<Esc>: Resume'
+        self.surface.blit(
+            tu.regular_text(colors.gray, footer_text),
+            (20, 480 - 20 - tu.line_size()),
         )
